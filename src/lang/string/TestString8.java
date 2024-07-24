9797afc4f703d8c8d72c30b4290d582ba7489691
DESKTOP-SN9D63J\JSH <tmdgus21@naver.com>
2024-07-24 17:25:15 +0900

    //Class

--- src/lang/string/TestString8.java
+++
@@ -1,9 +0,0 @@
-package lang.string;
-
-public class TestString8 {
-    public static void main(String[] args) {
-        String input = "hello";
-        String result = input.replace("hello", "world");
-        System.out.println(result);
-    }
-}
